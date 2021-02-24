42122e568cb2a089f33a6f2a97eb176e80563574
Martin Goode <martin.goode@awin.com>
2021-02-24 22:02:11 +0000

    REvert test changes

--- test/helpers/removeModel.js
+++ test/helpers/removeModel.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 module.exports = function removeModel(modelName) {
-	delete mongoose.models[modelName.toLowerCase().trim()];
-	delete mongoose.modelSchemas[modelName.toLowerCase().trim()];
+	delete mongoose.models[modelName];
+	delete mongoose.modelSchemas[modelName];
 };
